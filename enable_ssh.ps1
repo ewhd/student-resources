56cef1adc9ce7eeefe3803b56b8f6f6a25d68744
Ethan Denny <ewhd22@gmail.com>
2023-02-21 18:57:15 -0800

    Create enable_ssh.ps1

---
+++ enable_ssh.ps1
@@ -0,0 +1,15 @@
+# from https://woshub.com/connect-to-windows-via-ssh/
+
+# install ssh-server
+Get-WindowsCapability -Online | Where-Object Name -Like 'OpenSSH.Server*' | Add-WindowsCapability -Online
+
+# check installation
+Get-WindowsCapability -Online | ? name -Like 'OpenSSH.Ser*'
+
+# change the startup type of the sshd service to automatic and start the service using PowerShell:
+Set-Service -Name sshd -StartupType 'Automatic'
+Start-Service sshd
+
+#create firewall rule
+#Get-NetFirewallRule -Name *OpenSSH-Server* |select Name, DisplayName, Description, Enabled
+New-NetFirewallRule -Name sshd -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
